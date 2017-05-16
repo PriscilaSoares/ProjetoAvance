519a375c3bd20bf53032df40bc23e813ac286a10
tass2017 <talesasouza@gmail.com>
2017-05-15 21:00:59 -0300

    Add files via upload

---
+++ Scripts/Fase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class proxima : MonoBehaviour
+{
+    public string prox;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            SceneManager.LoadScene("level2");
+
+        }
+
+    }
+}

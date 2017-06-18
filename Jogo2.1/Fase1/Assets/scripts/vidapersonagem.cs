4eff4070aa261a5fdf1512fc0fcf062895657664
tales <talesasouza@gmail.com>
2017-06-18 20:03:25 -0300

    Jogo

---
+++ Jogo2.1/Fase1/Assets/scripts/vidapersonagem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vidapersonagem : MonoBehaviour {
+
+	public float barraCheia = 100f;
+	public float barraAtual;
+	public GameObject bar;
+
+	// Use this for initialization
+	void Start () {
+
+		barraCheia = barraAtual;
+	}
+
+	void OnCollisionEnter(Collision Collision){
+	
+		if(Collision.gameObject.tag == "inimigo1")
+		{
+			barraAtual = barraCheia - 10;
+		}
+	}
+
+
+
+
+
+	void Update () {
+
+
+
+
+	}
+
+}
